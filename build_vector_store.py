7ad16916047945d0e102429716b5db1c96ad4157
Utkarsh Misra <utkarshhoney8@gmail.com>
2025-12-18 21:10:12 +0530

    Refactor main function into build_vector_store

--- build_vector_store.py
+++ build_vector_store.py
@@ -6,14 +6,14 @@ from langchain_huggingface import HuggingFaceEmbeddings
 
 CHUNK_FILE = Path("data/chunks/chunks.json")
 VECTOR_DIR = Path("vector_store")
-VECTOR_DIR.mkdir(exist_ok=True)
 
-def main():
-    print("Loading chunks...")
+
+def build_vector_store():
+    VECTOR_DIR.mkdir(exist_ok=True)
+
+    print("📦 Loading chunks...")
     with open(CHUNK_FILE, "r", encoding="utf-8") as f:
         raw_chunks = json.load(f)
-
-    print(f"Chunks loaded: {len(raw_chunks)}")
 
     documents = [
         Document(page_content=c["text"], metadata=c["metadata"])
@@ -24,11 +24,12 @@ def main():
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
-    print("Building FAISS index...")
+    print("🔨 Building FAISS index...")
     vectorstore = FAISS.from_documents(documents, embeddings)
-
     vectorstore.save_local(VECTOR_DIR)
-    print("Vector store saved ✅")
+
+    print("✅ Vector store saved")
+
 
 if __name__ == "__main__":
-    main()
+    build_vector_store()
